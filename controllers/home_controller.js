4e6761826523403155b0796944a700e9d388ed55
Tushant8826 <tushantrock841@gmail.com>
2020-03-23 02:11:47 +0530

    setup static files access for layouts and pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home = function(req,res) {
-    return res.end('<h1>hi.....</h1>');
+    return res.render('home',{
+        title: "home"
+    })
 }
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');//step1
 const app = express();//step1
 const port = 8000;//step1
+const expressLayouts =  require('express-ejs-layouts');//step4 install express-ejs-layouts
 
 
+app.use(express.static('./assets'));//step5 
+app.use(expressLayouts);//step4 before routes
+app.set('layout extractStyles' ,true);//step6 extraxt style and scripts from sub pages into the layouts bascailly put link tage int head tag
+app.set('layout extractScripts' ,true);//step6
 app.use('/',require('./routes/index'));//step2 making routes and controllers
 app.set('view engine','ejs');//step3 install ejs it's a view template
 app.set('views','./views');//step3 set views folder
