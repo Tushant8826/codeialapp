3ca6cc871cbef97d38f0251e072dbbfc4a0712d5
Tushant8826 <tushantrock841@gmail.com>
2020-03-30 01:55:51 +0530

    coverting to ajax method insert post delete post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,29 +1,62 @@
-const Post  =  require('../models/post');
-const Comment  = require('../models/comment');
-module.exports.create =async function(req,res) {
-    try {
-        await Post.create({
-            content : req.body.content,
-            user : req.user._id
+const Post = require('../models/post');
+const Comment = require('../models/comment');
+
+module.exports.create = async function(req, res){
+    try{
+        let post = await Post.create({
+            content: req.body.content,
+            user: req.user._id
         });
+        
+        if (req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
+
+        req.flash('success', 'Post published!');
         return res.redirect('back');
+
     }catch(err){
-        console.log('Error in creatin in post',err);
-        return;
-    }  
+        req.flash('error', err);
+        return res.redirect('back');
+    }
+  
 }
-module.exports.destroy =async function(req,res) {
-  try {
-    let post= await Post.findById(req.params.id);
-    if(post.user==req.user.id) {
-        post.remove();
-        await Comment.deleteMany({post : req.params.id});
+
+
+module.exports.destroy = async function(req, res){
+
+    try{
+        let post = await Post.findById(req.params.id);
+
+        if (post.user == req.user.id){
+            post.remove();
+
+            await Comment.deleteMany({post: req.params.id});
+            if (req.xhr){
+              return res.status(200).json({
+                  data: {
+                      post_id: req.params.id
+                  },
+                  message: "Post deleted"
+              });
+          }
+
+            req.flash('success', 'Post and associated comments deleted!');
+
+            return res.redirect('back');
+        }else{
+            req.flash('error', 'You cannot delete this post!');
+            return res.redirect('back');
+        }
+
+    }catch(err){
+        req.flash('error', err);
         return res.redirect('back');
-    }else {
-        return res.redirect('back');
-    } 
-  }catch(err) {
-      console.log('Error in destroy of post',err);
-      return;
-  }
+    }
+    
 }
